e6af4c3b518d11ee8d58c097b2122c83d9c4a558
Adrian <adrianlayson0@gmail.com>
2025-04-11 15:40:05 +0800

    instead of choices 1, I changed into alphabet since I think it's more pleasing and aesthetic

--- oop_assignment_9/quiz_inputter.py
+++ oop_assignment_9/quiz_inputter.py
@@ -6,19 +6,24 @@ quizzes = []
 
 while True:
     question = input("Enter your question: ")
-    choices = [input(f"Choice {i + 1}: " ) for i in range(4)]
-    correct = int(input(" Which one is correct? (1 - 4): "))
+    choices = []
+    for i in range(4):
+        choice = input(f"Choice {chr(65 + i)}: ")
+        choices.append(choice)
+
+    correct = input("Which one is correct? (A - D): ").upper()
     
     quiz = f"Q: {question}\n"
-    for i, choice in enumerate(choices, 1):
-        quiz += f"{i}.{choice}\n"
+    letters = ["A", "B", "C", "D"]
+    for i, choice in enumerate(choices):
+        quiz += f"{letters[i]}. {choice}\n"
     quiz += f"Answer: {correct}\n\n"
 
     quizzes.append(quiz)
 
-    again = input("Wil you add another question? (y/n): ").lower()
+    again = input("Will you add another question? (y/n): ").lower()
     if again != 'y':
-        print("Exiting program...")
+        print("\nExiting program...\n")
         break
 
 with open("quiz_entries.txt", "a") as file:

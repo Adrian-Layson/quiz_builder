be3aa59ba68ead681837253358b4f80521948dc6
Adrian <adrianlayson0@gmail.com>
2025-04-11 14:25:28 +0800

    initialize the loop that asks the user for question. choices and andswer

--- quiz_inputter.py
+++ quiz_inputter.py
@@ -1,3 +1,10 @@
 print("=== WELCOME TO QUIZ BUILDER ===")
 
 quizzes = []
+
+while True:
+    question = input("Enter your question: ")
+    choices = [input(f"Choice {i + 1}") for i in range(4)]
+    correct = int(input(" Which one is correct? (1 - 4): "))
+
+    

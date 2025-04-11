4d00f7eae0489150ca1ca301a08bee2f998a958d
Adrian <adrianlayson0@gmail.com>
2025-04-11 14:20:38 +0800

    initiated a code that prints the title of my program

--- quiz_inputter.py
+++ quiz_inputter.py
@@ -1,12 +1,1 @@
-# Print title of the program
-
-#Initialize a loop that:
-    # Ask for question and 4 choices
-    # Ask which choice is correct
-    # Format and add the quiz to the list
-    # Ask if user wants to add another
-    #If not, stop
-
-# Write all quiz entries to a text file
-# Show "Quiz saved"
-# End program
+print("=== WELCOME TO QUIZ BUILDER ===")

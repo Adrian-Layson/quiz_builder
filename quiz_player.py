b17d14fe33fd745aba840b450bc49c21fe54c9b3
Adrian <adrianlayson0@gmail.com>
2025-05-09 16:18:33 +0800

    add function to load and inspect quizzes from file

--- quiz_player.py
+++ quiz_player.py
@@ -1,2 +1,9 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
+
+def load_quizzes(filename):
+    with open(filename, "r") as file:
+        content = file.read().strip()
+
+    raw_quizzes = content.split("\n\n")
+    quizzes = []

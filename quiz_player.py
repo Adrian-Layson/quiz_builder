601ce29c1ed9b64d052c11217acb9b59677a1a7d
Adrian <adrianlayson0@gmail.com>
2025-05-09 16:22:16 +0800

    process and structure quiz data from txt file

--- quiz_player.py
+++ quiz_player.py
@@ -7,3 +7,13 @@ def load_quizzes(filename):
 
     raw_quizzes = content.split("\n\n")
     quizzes = []
+
+    for raw in raw_quizzes:
+        lines = raw.strip().split("\n")
+        if len(lines) < 6:
+            continue
+        question = lines[0][3:]
+        choices = [line[3:] for line in lines[1:5]]  # Remove "A. ", etc.
+        correct = lines[5].split(":")[1].strip()
+        quizzes.append((question, choices, correct))
+    return quizzes
